cec0dcb68f7adfb2be3857fc784d19e5a6e6d29c
Claudio Junior Familia Morel <claudio.familia.morel@gmail.com>
2020-05-14 09:13:10 -0400

    Innitial commit

--- yofacturoapp/src/App.vue
+++ yofacturoapp/src/App.vue
@@ -1,52 +1,33 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <!-- Config of general aspects of the app -->
+    <CoreFilter />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+    <!-- Tool Upper bar -->
+    <CoreToolbar />
 
-      <v-spacer></v-spacer>
+    <!-- SideBar -->
+    <CoreDrawer />
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld />
-    </v-content>
+    <!-- Where routes get show and footer -->
+    <CoreView />
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import CoreFilter from "./components/core/Filter";
+import CoreDrawer from "./components/core/Drawer";
+import CoreToolbar from "./components/core/ToolUpperBar";
+import CoreView from "./components/core/View";
 
 export default {
   name: "App",
 
   components: {
-    HelloWorld
+    CoreFilter,
+    CoreDrawer,
+    CoreToolbar,
+    CoreView
   },
 
   data: () => ({
@@ -54,3 +35,13 @@ export default {
   })
 };
 </script>
+
+
+<style lang="scss">
+@import '@/styles/index.scss';
+
+/* Remove in 1.2 */
+.v-datatable thead th.column.sortable i {
+  vertical-align: unset;
+}
+</style>
---
+++ yofacturoapp/src/components/core/View.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-content>
+    <div id="core-view">
+      <v-fade-transition mode="out-in">
+        <router-view />
+      </v-fade-transition>
+    </div>
+    <core-footer />
+  </v-content>
+</template>
+
+<script>
+import CoreFooter from './Footer'
+
+export default {
+  metaInfo () {
+    return {
+      title: 'Yo Facturo App',
+      description: 'Aplicacion para manejo de facturas y inventario'
+    }
+  },
+  components: {
+      CoreFooter
+  }
+}
+</script>
+
+<style>
+#core-view {
+  padding-bottom: 100px;
+}
+</style>
